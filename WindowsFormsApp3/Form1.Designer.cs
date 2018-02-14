0084587bbf5d01a9a11559839c8c4d2e3bef9d78
Ayugradow <32333973+Ayugradow@users.noreply.github.com>
2018-02-14 14:18:12 -0200

    First commit

---
+++ WindowsFormsApp3/Form1.Designer.cs
@@ -0,0 +1,195 @@
+﻿namespace WindowsFormsApp3
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.openFileDialog2 = new System.Windows.Forms.OpenFileDialog();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            this.openFileDialog1.Filter = "\"Mod files|*.dll|All files|*.*\"";
+            this.openFileDialog1.Title = "Select the modding API (if it\'s not already installed)";
+            // 
+            // openFileDialog2
+            // 
+            this.openFileDialog2.DefaultExt = "dll";
+            this.openFileDialog2.FileName = "openFileDialog2";
+            this.openFileDialog2.Filter = "\"Mod files|*.dll|All files|*.*\"";
+            this.openFileDialog2.Multiselect = true;
+            this.openFileDialog2.Title = "Select the mods you wish to install";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.button1);
+            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(327, 106);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(3, 40);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(61, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Install path:";
+            // 
+            // button1
+            // 
+            this.button1.AllowDrop = true;
+            this.button1.Location = new System.Drawing.Point(37, 11);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(248, 26);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Select your Hollow Knight installation path";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click_1);
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.label3);
+            this.groupBox2.Controls.Add(this.label2);
+            this.groupBox2.Controls.Add(this.button3);
+            this.groupBox2.Controls.Add(this.button2);
+            this.groupBox2.Location = new System.Drawing.Point(12, 124);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(327, 211);
+            this.groupBox2.TabIndex = 1;
+            this.groupBox2.TabStop = false;
+            // 
+            // label3
+            // 
+            this.label3.AutoEllipsis = true;
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(3, 118);
+            this.label3.MaximumSize = new System.Drawing.Size(327, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(73, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Selected files:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(3, 44);
+            this.label2.MaximumSize = new System.Drawing.Size(200, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(68, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Selected file:";
+            // 
+            // button3
+            // 
+            this.button3.AllowDrop = true;
+            this.button3.Location = new System.Drawing.Point(37, 89);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(248, 26);
+            this.button3.TabIndex = 1;
+            this.button3.Text = "Select the mods you wish to install";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button2
+            // 
+            this.button2.AllowDrop = true;
+            this.button2.AutoSize = true;
+            this.button2.Location = new System.Drawing.Point(37, 15);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(253, 26);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Select the modding API (if it\'s not already installed)";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click_1);
+            // 
+            // button4
+            // 
+            this.button4.Enabled = false;
+            this.button4.Location = new System.Drawing.Point(240, 346);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(99, 23);
+            this.button4.TabIndex = 2;
+            this.button4.Text = "Install";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(351, 381);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Hollow Knight Mod Installation Wizard";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog1;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.OpenFileDialog openFileDialog2;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button4;
+    }
+}
+
---
+++ WindowsFormsApp3/Form1.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp3
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+            folderBrowserDialog1.ShowDialog();
+            label1.Text = "Install path: \n" + folderBrowserDialog1.SelectedPath;
+            button4.Enabled = ((folderBrowserDialog1.SelectedPath != "folderBrowserDialog1") && (openFileDialog2.FileName != "openFileDialog2"));
+        }
+
+        private void button2_Click_1(object sender, EventArgs e)
+        {
+            openFileDialog1.ShowDialog();
+            if (openFileDialog1.FileName != "openFileDialog1")
+            label2.Text = "Selected file: \n" + openFileDialog1.FileName;
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            openFileDialog2.ShowDialog();
+            if (openFileDialog2.FileName != "openFileDialog2")
+            {
+                List<string> mods = new List<string>();
+                foreach (string mod in openFileDialog2.FileNames)
+                {
+                    mods.Add(System.IO.Path.GetFileName(mod));
+                }
+                label3.Text = "Selected file(s): \n" + String.Join("\n", mods.ToArray());
+                button4.Enabled = ((folderBrowserDialog1.SelectedPath != "folderBrowserDialog1") && (openFileDialog2.FileName != "openFileDialog2"));
+            }
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            foreach (string mod in openFileDialog2.FileNames)
+            {
+                System.IO.File.Copy(mod, System.IO.Path.Combine(folderBrowserDialog1.SelectedPath + "/hollow_knight_data/managed/Mods", System.IO.Path.GetFileName(mod)), true);
+            }
+            if (openFileDialog1.FileName == "openFileDialog1")
+            MessageBox.Show("Succesfully installed mods!");
+            else
+            {
+                System.IO.File.Copy(openFileDialog1.FileName, System.IO.Path.Combine(folderBrowserDialog1.SelectedPath + "/hollow_knight_data/managed", System.IO.Path.GetFileName(openFileDialog1.FileName)), true);
+                MessageBox.Show("Succesfully installed API and mods!");
+            }
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            MessageBox.Show("Use this application to install API mods.\nNavigate to your Steam/GOG Hollow Knight installation path using the first button.\nUsing the second button you can select the Modding API and using the third button you can select which mods to install.\nAfter that, simply click the Install button and it should automatically install everything in the correct paths.");
+        }
+    }
+}
